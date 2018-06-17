8b585bdb889457df8cba77b4b383885de3230619
Jordan A. Earnest <jordanearnest1@gmail.com>
2018-06-17 19:00:00 -0400

    first run of scraping is in place, in process of writing to xl

--- worldcat/worldcat.py
+++ worldcat/worldcat.py
@@ -6,25 +6,17 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 
+from openpyxl import *
+
+import openpyxl
+
 # api_key = secrets.api_key
-
-
-
-# FNAME = "master_copy_harvard_hebraic_collection_61418.csv"
-
 
 FNAME = "Judeo_arabic_spreadsheet.xlsx"
 
-def load_excel_file(FNAME):
-	xl = pd.ExcelFile(FNAME)
-	df1 = xl.parse('Judeo-Arabic')
-	return df1
-	# for a_title in df1['Title']:
-	# 	print(a_title)
 
 
-##later can add more self definitions for other categories
-class Document:
+class Document():
 	def __init__(self, title):
 		self.title = title
 		self.subject_tags = ""
@@ -34,29 +26,80 @@ class Document:
 	pass
 
 
-# example 
-#https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah
+
+# def load_excel_file(FNAME):
+# 	xl = pd.ExcelFile(FNAME)
+# 	df = xl.parse('Judeo-Arabic')
+# 	return df
+
+def load_excel_file(FNAME):
+	wb = load_workbook(filename = FNAME)
+	sheet_ranges = wb['Judeo-Arabic']
 
 
-example_title = "[Aseret ha-devarim] divre Elohim hayim ha-neemarim...ba-ir Tunis... : targum ve-Aravi laTorah"
+
+	print(sheet_ranges['H18'].value)
 
 
-gold_standard = "https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah/"
-example_title = example_title.replace('[', ' ').replace(']', ' ').replace('.', ' ').replace(':', ' ').split()
+#load_excel_file(FNAME)	
+
+#current_number = which cell we're on
+
+current_number = 2  ## to start with 
+cell_number = "H" + str(current_number)
+# print(cell_number)
+current_number += 1
+
+
+
+def write_to_excel(filename, cell_number):
+	xfile = openpyxl.load_workbook(filename)
+	sheet = xfile.get_sheet_by_name('Judeo-Arabic')
+	print("herewara")
+	print(sheet)
+	print(sheet[cell_number])
+
+write_to_excel(FNAME, cell_number)
+
+
+
+	# xl = load_workbook(FNAME)
+
+
+	# df = xl.parse('Judeo-Arabic')
+	# return df
+
+
+
+
+# dataframe = load_excel_file(FNAME)
+# for column in dataframe["Title"]:
+# 	print(column)
+
+
+
+
+
+
+# example_title = "[Aseret ha-devarim] divre Elohim hayim ha-neemarim...ba-ir Tunis... : targum ve-Aravi laTorah"
+
+# example_title = "Shulḥan ʻarukh Oraḥ ḥayim bil-ʻArabi : [ʻim perush Kaf ha-ḥayim ... siman 157[-231]"
+
+# gold_standard = "https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah/"
 
 
 
 def assemble_url(title):
+	title= title.replace('[', ' ').replace(']', ' ').replace('.', ' ').replace(':', ' ').replace("ʻ", "").split()
 	baseurl = "https://www.worldcat.org/title/"
-	title_concat_string = ""
+	title_concat_string = ''
 	for word in title:
 		title_concat_string += word + "-"
-	title_concat_string = title_concat_string[:-1]
+
+	title_concat_string = title_concat_string[:-1].replace("--", '-')
 	complete_url = baseurl + title_concat_string
 	print(complete_url)
 	return complete_url
-
-
 
 
 def make_request(complete_url):
@@ -71,23 +114,20 @@ def make_request(complete_url):
 		subject_list = []
 
 		for line in subject_div:
-			subjects = line.text.replace('-', "").replace(".", "").split()
+			subjects = line.text.replace('--', "").replace(".", "").split()
 
 			for item in subjects:
 				if item not in subject_list:
 					subject_list.append(item)
-			print(subject_list)
+			return subject_list
 
 	else:
 		print("### BAD STATUS CODE ###")
 
 
 
-url = assemble_url(example_title)
-
-soup = make_request(url)
-
-
+# url = assemble_url(example_title)
+# soup = make_request(url)
 
 
 
@@ -107,7 +147,6 @@ def get_given_title(FNAME):
 		# for column in csvfile:
 		# 	print(', '.join(str(column)))
 	pass
-
 
 def close_excel():
 	pass
