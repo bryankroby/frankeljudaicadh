316a5c9c0d4e1a4bf92b04b57b1cc6b93f204c9e
Jordan A. Earnest <jordanearnest1@gmail.com>
2018-06-17 17:32:55 -0400

    finally able to load in xmlx file without byte encoding errors. test scraped WorldCat.org for subject tags

--- worldcat/worldcat.py
+++ worldcat/worldcat.py
@@ -2,9 +2,25 @@ import json
 import requests
 import csv
 import secrets as secrets
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+# api_key = secrets.api_key
 
 
-api_key = secrets.api_key
+
+# FNAME = "master_copy_harvard_hebraic_collection_61418.csv"
+
+
+FNAME = "Judeo_arabic_spreadsheet.xlsx"
+
+def load_excel_file(FNAME):
+	xl = pd.ExcelFile(FNAME)
+	df1 = xl.parse('Judeo-Arabic')
+	return df1
+	# for a_title in df1['Title']:
+	# 	print(a_title)
 
 
 ##later can add more self definitions for other categories
@@ -18,37 +34,86 @@ class Document:
 	pass
 
 
-def get_params():
-	pass
+# example 
+#https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah
 
 
-def make_request(baseurl, params):
-	print("Making a reuqest for new data...")
-	resp = requests.get(baseurl, params)
-	loaded_text = json.loads(resp.text)
-	return loaded_text
-	pass
+example_title = "[Aseret ha-devarim] divre Elohim hayim ha-neemarim...ba-ir Tunis... : targum ve-Aravi laTorah"
+
+
+gold_standard = "https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah/"
+example_title = example_title.replace('[', ' ').replace(']', ' ').replace('.', ' ').replace(':', ' ').split()
+
+
+
+def assemble_url(title):
+	baseurl = "https://www.worldcat.org/title/"
+	title_concat_string = ""
+	for word in title:
+		title_concat_string += word + "-"
+	title_concat_string = title_concat_string[:-1]
+	complete_url = baseurl + title_concat_string
+	print(complete_url)
+	return complete_url
+
+
+
+
+def make_request(complete_url):
+	print("Making a request for new data...")
+	resp = requests.get(complete_url)
+
+	print(resp.status_code)
+	if resp.status_code == 200:
+		resp = resp.content
+		soup = BeautifulSoup(resp, 'html.parser')
+		subject_div = soup.find_all(id = "subject-terms")
+		subject_list = []
+
+		for line in subject_div:
+			subjects = line.text.replace('-', "").replace(".", "").split()
+
+			for item in subjects:
+				if item not in subject_list:
+					subject_list.append(item)
+			print(subject_list)
+
+	else:
+		print("### BAD STATUS CODE ###")
+
+
+
+url = assemble_url(example_title)
+
+soup = make_request(url)
+
+
+
+
 
 
 def assign_class(loaded_text):
 	pass
 
 
-def get_given_title():
+def get_given_title(FNAME):
+	# with open(FNAME, 'rb') as csvfile:
+	# 	contents = csv.reader(csvfile)
+	# 	next(contents)  # skip header
+	# 	data = [r for r in reader]
+	# 	print(data[0])
 
-	return title
 
+		# for column in csvfile:
+		# 	print(', '.join(str(column)))
+	pass
 
-def open_excel():
-	with 
 
 def close_excel():
 	pass
 
 
-def get_given_title():
-	csv = open_excel()
-	for document_item in csv:
+
 
 
 
