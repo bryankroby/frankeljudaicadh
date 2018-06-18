c6f3be9c6516e880e14735e94e62dc411a17e753
Jordan A. Earnest <jordanearnest1@gmail.com>
2018-06-18 11:13:43 -0400

    ready to do first testdrive of entire process, but first need to reformat subject_list out of python lst

--- worldcat/worldcat.py
+++ worldcat/worldcat.py
@@ -25,95 +25,16 @@ class Document():
 		return subject_tags
 	pass
 
-
-# def load_excel_file(FNAME):
-# 	xl = pd.ExcelFile(FNAME)
-# 	df = xl.parse('Judeo-Arabic')
-# 	return df
-
-
-def iterate_excel_file(FNAME):
-	wb = load_workbook(filename = FNAME)
-	sheet = wb['Judeo-Arabic']
-	print("HIGHEST ROW")
-	print(sheet.max_row)
-	print("HIGHEST COLUMN")
-	print(sheet.max_column)
-
-	current_number = 2  ## to start with 
-	cell_number = "H" + str(current_number)
-
-	while sheet[cell_number].value != None:
-		print("cell number:")
-		print(cell_number)
-		print("cell value")
-		print(sheet[cell_number].value)
-		current_number += 1
-		cell_number = "H" + str(current_number)
-
-	return sheet
-
-
-
-def iterate_rows(sheet):
-	
-
-	# for row in sheet["H18"]:
-	# 	print(row.value)
-
-
-
-	for row in sheet.iter_cols(min_row = 2, min_col = 8, max_row = 313, max_col= 8):
-		for cell in row:
-			print(cell.value)
-
-
-
-iterate_excel_file(FNAME)
-
-# the_current_sheet = load_excel_file(FNAME)
-# iterate_rows(the_current_sheet)
-
-
-
-
-
-
-def write_to_excel(filename, cell_number):
+def write_to_excel(filename, current_number, subjects):
 	xfile = openpyxl.load_workbook(filename)
 	sheet = xfile.get_sheet_by_name('Judeo-Arabic')
-
-	print(sheet[cell_number].value)
-	sheet["AC2"] = "THIS is a test!"
+	write_to_cell_number = "AD" + str(current_number)
+	#print(sheet[write_to_cell_number].value)
+	sheet[write_to_cell_number] = subjects
 	xfile.save(FNAME)
 
-
-
-
-# write_to_excel(FNAME, cell_number)
-
-	# xl = load_workbook(FNAME)
-
-	# df = xl.parse('Judeo-Arabic')
-	# return df
-
-
-# dataframe = load_excel_file(FNAME)
-# for column in dataframe["Title"]:
-# 	print(column)
-
-
-
-# example_title = "[Aseret ha-devarim] divre Elohim hayim ha-neemarim...ba-ir Tunis... : targum ve-Aravi laTorah"
-
-# example_title = "Shulḥan ʻarukh Oraḥ ḥayim bil-ʻArabi : [ʻim perush Kaf ha-ḥayim ... siman 157[-231]"
-
-# gold_standard = "https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah/"
-
-
-
 def assemble_url(title):
-	title= title.replace('[', ' ').replace(']', ' ').replace('.', ' ').replace(':', ' ').replace("ʻ", "").split()
+	title= title.replace('[', ' ').replace(']', ' ').replace('.', ' ').replace(':', ' ').replace("ʻ", "").replace("(", "").replace(")", "").split()
 	baseurl = "https://www.worldcat.org/title/"
 	title_concat_string = ''
 	for word in title:
@@ -123,7 +44,6 @@ def assemble_url(title):
 	complete_url = baseurl + title_concat_string
 	print(complete_url)
 	return complete_url
-
 
 def make_request(complete_url):
 	print("Making a request for new data...")
@@ -143,9 +63,63 @@ def make_request(complete_url):
 				if item not in subject_list:
 					subject_list.append(item)
 			return subject_list
-
 	else:
 		print("### BAD STATUS CODE ###")
+		return False
+
+
+def iterate_excel_file(FNAME):
+	wb = load_workbook(filename = FNAME)
+	sheet = wb['Judeo-Arabic']
+	current_number = 2  ## to start with 
+	count_times_written = 0
+	count_times_NOT_written = 0 
+	title_cell_number = "H" + str(current_number)
+
+	while sheet[title_cell_number].value != None:
+		print(title_cell_number)
+		current_title = sheet[title_cell_number].value  
+
+		##pass the title into assemble_url, which makes it into a url can make request with
+		assembled_url = assemble_url(current_title)
+
+		##making the request with assembled URL, if there's an error, will return false
+		result = make_request(assembled_url) 
+
+		if result != False:
+			subjects = result
+			write_to_excel(FNAME, current_number, subjects)
+			count_times_written +=1 
+		else:
+			count_times_NOT_written += 1
+
+		current_number += 1
+		title_cell_number = "H" + str(current_number)
+
+	print("Times written = ")
+	print(count_times_written)
+	print("Times NOT written = ")
+	print(count_times_NOT_written)
+
+	return sheet
+
+iterate_excel_file(FNAME)
+
+
+
+
+
+# example_title = "[Aseret ha-devarim] divre Elohim hayim ha-neemarim...ba-ir Tunis... : targum ve-Aravi laTorah"
+
+# example_title = "Shulḥan ʻarukh Oraḥ ḥayim bil-ʻArabi : [ʻim perush Kaf ha-ḥayim ... siman 157[-231]"
+
+# gold_standard = "https://www.worldcat.org/title/aseret-ha-devarim-divre-elohim-hayim-ha-neemarim-ba-ir-tunis-targum-ve-aravi-latorah/"
+
+
+
+
+
+
 
 
 
